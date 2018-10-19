185116b29984f28d91e08daec6c4120202471d4d
Ryan Daley <rdaley321@yahoo.com>
2018-10-19 03:35:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     cart.splice(0,cart.length);
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
 }
