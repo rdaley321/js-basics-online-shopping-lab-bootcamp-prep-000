6927fe914671cf5227a6fef89c60a63d3e52f1b6
Ryan Daley <rdaley321@yahoo.com>
2018-10-19 03:22:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,14 +46,19 @@ function total() {
 function removeFromCart(item) {
   for(let i=0;i<cart.length;i++) {
     if(item === cart[i].itemName) {
-      cart.splice(i,1)
-      return cart
+      cart.splice(i,1);
+      return cart;
     } 
   }
-  return "That item is not in your cart."
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
   // write your code here
-  if(cardNumber===)
+  if(cardNumber===undefined) {
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    cart.splice(0,cart.length);
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  }
 }
