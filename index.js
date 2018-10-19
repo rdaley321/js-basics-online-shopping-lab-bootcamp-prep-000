7903309bcd4a77646acf4aa07c0f9c03534c2dd0
Ryan Daley <rdaley321@yahoo.com>
2018-10-19 03:25:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function placeOrder(cardNumber) {
   if(cardNumber===undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    let total = total()
+    total();
     cart.splice(0,cart.length);
     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
   }
